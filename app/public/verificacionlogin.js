cbdfef9e32a721264048a54f6d566a9bf9b873e3
jadeemttz <jmartinezh2211@alumno.ipn.mx>
2025-04-02 06:33:05 -0600

    desvanecimiento en alertas de verificacion login

--- app/public/verificacionlogin.js
+++ app/public/verificacionlogin.js
@@ -2,7 +2,9 @@ window.addEventListener('load', () => {
     const body = document.body;
     body.style.opacity = '1';
 });
-
+function esperar(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 document.addEventListener('DOMContentLoaded', () => {
     const reenviarBtn = document.getElementById('reenviar-codigo');
     const codigoForm = document.getElementById('codigo-contraseña-form');
@@ -59,7 +61,20 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log("Respuesta del servidor:", result);
 
             if (response.ok && result.status === 'ok') {
-                window.location.href = result.redirect;
+                submitBtn.disabled = true;
+                mostraralerta('success',"Correo verificado exitosamente");
+                // Esperar 4 segundos (4000 ms) antes de cerrar la alerta y redirigir
+                await esperar(4000); // Espera 4 segundos
+
+            // Hacer la animación de desvanezca del body
+             document.body.style.transition = 'opacity 0.5s';
+             document.body.style.opacity = '0'; // Opcional: transición de desvanezca
+
+            // Esperar a que la animación termine antes de redirigir
+            await esperar(500); // Esperar el tiempo de la animación (500 ms)
+            cerraralerta();
+            window.location.href = resJson.redirect;
+
             } else {
                 mostraralerta("error", 'Error al validar el código');
             }
