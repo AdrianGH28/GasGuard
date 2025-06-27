5289a16b65a8840c4ce71b6f6a56695d211d2cb1
MushroomZero <bcruze2200@alumno.ipn.mx>
2025-06-27 04:09:24 -0600

    porfa neta ya

--- app/public/MAE_visualizarrep.js
+++ app/public/MAE_visualizarrep.js
@@ -6,16 +6,8 @@ window.addEventListener('load', () => {
 
 const modal = document.querySelector('#modal');
 const pageContainer = document.querySelector('.page-container');
-const addca = document.getElementById('anadircuenta');
-const cerrarmodal = document.querySelector('#cerrarmodal')
 
-// Función para mostrar u ocultar el modal
-addca.addEventListener('click', () => {
-        modal.showModal();
-});
-cerrarmodal.addEventListener('click', () => {
-    modal.close();
-});
+
 function esperar(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
